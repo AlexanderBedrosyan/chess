15e0f08f0ccc2e0c8b47e1161ac6e90530c904e4
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-02-27 16:27:37 +0200

    Add new logic in the history function

--- my_chess_game/history_of_games.py
+++ my_chess_game/history_of_games.py
@@ -1,4 +1,6 @@
 import requests
+import chess.pgn
+import io
 
 
 class HistoryOfGames:
@@ -16,13 +18,45 @@ class HistoryOfGames:
             if response.status_code == 200:
                 data = response.json()
                 for game in data.get("games", []):
-                    games.append(game["pgn"])
-                    print(game["pgn"])
+                    pgn_text = game["pgn"]
+                    fens = self.get_fen_positions(pgn_text)
+                    games.append(fens)
+                    # print(game["pgn"])
+                    print(fens)
+                    exit()
             else:
                 print(f"Грешка {response.status_code}: {response.text}")
 
         return games
 
+    def get_fen_positions(self, pgn_text):
+        fen_positions = []
+        game = chess.pgn.read_game(io.StringIO(pgn_text))
+        if not game:
+            return fen_positions
+
+        board = game.board()
+        fen_positions.append(board.fen())
+
+        for move in game.mainline_moves():
+            board.push(move)
+            fen_positions.append(board.fen())
+
+        return fen_positions
+
+    def find_matching_games(self, current_fen):
+        games = self.find_all_games()
+        matching_games = []
+
+        for game in games:
+            fens = self.get_fen_positions(game)
+            if current_fen in fens:
+                matching_games.append(game)
+
+        return matching_games
+
 
 history_of_games = HistoryOfGames()
-history_of_games.find_all_games()
+print(history_of_games.find_all_games())
+current_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+print(history_of_games.find_matching_games(current_fen))
